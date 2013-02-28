64301e4d3223b1e5d990fe86b8c2c2d824c6d6ae
Jerome Lacoste <jerome.lacoste@gmail.com>
2013-02-28 07:13:40 +0100

    Improve word frequency by removing non characters, downcasing everything and removing words less than 3 characters

--- wordfrequency.rb
+++ wordfrequency.rb
@@ -1,4 +1,4 @@
-words = File.open(ARGV[0]) {|f| f.read }.split 
+words = File.open(ARGV[0]) {|f| f.read }.split(/[^\w]/).map{|w|w.downcase}.select {|w| w.length > 2}
 freqs=Hash.new(0) 
 words.each { |word| freqs[word] += 1 } 
 freqs.sort_by {|x,y| y }.reverse.each {|w, f| puts w+' '+f.to_s} 
